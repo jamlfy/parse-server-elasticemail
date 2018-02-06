7aa7f2568e011584c4441fe993a705ee8580a0e8
Alejo Next <alejonext@gmail.com>
2018-02-06 12:53:31 -0500

    New Module Parser-Sever

---
+++ index.js
@@ -0,0 +1,85 @@
+const ElasticEmail = require("elastic-email-client");
+
+const ERRORS = {
+	bad_template_config : 'ElasticEmailAdapter templates are not properly configured.',
+	missing_api_key : 'ElasticEmailAdapter requires valid API Key.',
+	bad_recipient : 'ElasticEmailAdapter messing recipient in the template '
+};
+
+
+class MailAdapter {
+	constructor(options){
+
+		if (!options.apiKey) {
+			throw new Error(ERRORS.missing_api_key);
+		}
+
+		if (!options.templates || Object.keys(options.templates).length === 0) {
+			throw new Error(ERRORS.bad_template_config);
+		}
+
+		for (let name in options.templates) {
+			if(typeof options.templates[name] != 'string' && typeof options.templates[name] != 'object' ){
+				throw new Error(ERRORS.bad_template_config);
+			}
+		}
+
+		this.config = options;
+		this.elastic = new ElasticEmail(options.apiKey);
+	}
+
+	_senderMail(opts, isDirect){
+		var mesg = { template : opts.template };
+
+		if(isDirect){
+			const { subject, fromAddress, recipient, variables, extra } = options;
+
+			if (!recipient) {
+				throw new Error(ERRORS.bad_recipient + mesg.template);
+			}
+
+			mesg.from = options.fromAddress || (this.config.template[mesg.template] && this.config.template[mesg.template].subject) || this.config.fromAddress;
+			mesg.to = recipient;
+			mesg.subject = subject || (this.config.template[mesg.template] && this.config.template[mesg.template].subject);
+
+			mesg = Object.assign(mesg, extra || {});
+		} else {
+			mesg.from = (this.config.template[mesg.template] && this.config.template[mesg.template].subject) || this.config.fromAddress;
+			mesg.to = options.user.get('email');
+			mesg.subject = (this.config.template[mesg.template] && this.config.template[mesg.template].subject);
+			mesg.merge_link = options.link;
+			mesg.merge_appName = options.appName;
+			mesg.merge_user = options.user;
+		}
+
+		if(opts.variables){
+			for (var i in opts.variables ) {
+				mesg['merge_' + i ] = opts.variables[i];
+			}
+		}
+		// Clean
+		for (var i in mesg) {
+			if(!mesg[i]){
+				delete mesg[i];
+			}
+		}
+
+		return this.elastic.Email.Send(mesg);
+	}
+
+	sendPasswordResetEmail(variables) {
+		let template = ( this.config.templates.passwordResetEmail && this.config.templates.passwordResetEmail.template ) || this.config.templates.passwordResetEmail || 'passwordResetEmail'
+		return this._sendMail({ template, variables });
+	}
+
+	sendPasswordResetEmail(variables) {
+		let template = ( this.config.templates.verificationEmail && this.config.templates.verificationEmail.template ) || this.config.templates.verificationEmail || 'passwordResetEmail'
+		return this._sendMail({ template, variables });
+	}
+
+	send({ template, subject, fromAddress, recipient, variables, extra }) {
+		return this._sendMail({ template, subject, fromAddress, recipient, variables, extra }, true);
+	}
+}
+
+module.exports = MailAdapter;
